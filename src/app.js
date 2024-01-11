72ef01481b3ff7ba4ad9feec32d0fc9fa7ddb910
vvm1004 <96302642+vvm1004@users.noreply.github.com>
2024-01-11 23:45:34 +0700

    init project vs install package

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const morgan = require('morgan')
+const {default: helmet} = require('helmet')
+const compression = require('compression')
+
+
+const app = express()
+
+//init middlewares
+app.use(morgan("dev"))
+// app.use(morgan("combined"))
+// morgan("common")
+// morgan("short")
+// morgan("tiny")
+
+app.use(helmet())
+app.use(compression())
+
+//init db
+
+//init routes
+app.get('/', (req, res, next) => {
+    const strCompress = 'Hello Factipjs'
+    return res.status(200).json({
+        message: 'Welcome Fantipjs!',
+        metadata: strCompress.repeat(100000)
+    })
+})
+//handling error
+
+
+module.exports = app
